4384b892bf24487e1f793f7002c1f1460e381f5e
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-09-18 15:52:10 +1000

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const init = () => {
-  
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+      
+        console.log(input.value);
+      }); 
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
